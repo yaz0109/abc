636a83a0946e948b8dbd3fe76d89c9fd79522e70
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2022-04-01 14:28:21 -0700

    mapa

--- MyApp.py
+++ MyApp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-df = pd.read_csv('https://raw.githubbusercontent.com/quantum-apps/mapa/main/data.csv') 
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv') 
 st.write(df)
 st.map(df)
 st.title ("Mi primera App")

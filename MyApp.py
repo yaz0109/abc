a401fce53a23f988e95b3f8569d6305e80d556b3
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2022-04-01 14:21:36 -0700

    agregue panda

--- MyApp.py
+++ MyApp.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+df = pd.read_csv('https://raw.githubbusercontent.com/quantum-apps/mapa/main/data.csv') 
+st.write(df)
+st.map(df)
 st.title ("Mi primera App")
 #st.button("dale click")
 #st.button("otro boton")

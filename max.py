8b91e9f806b8529efbaa0f8400e80c33ceb43e30
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 10:26:35 -0700

    Update max.py
    a

--- max.py
+++ max.py
@@ -8,7 +8,7 @@ def main():
     st.title("Challenging situation")
     st.write("Pipe network stronghold")
     st.write("Pipe network design")
-texto_ingresado = st.text_input("Ingrese su texto", "Texto predeterminado")
+    texto_ingresado = st.text_input("Ingrese su texto", "Texto predeterminado")
     st.write("Texto ingresado:", texto_ingresado)
     
     

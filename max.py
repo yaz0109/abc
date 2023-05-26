2b6fd34c8f3be3e3191a6db0f41a244d09251a86
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 10:44:09 -0700

    Update max.py
    op

--- max.py
+++ max.py
@@ -9,6 +9,18 @@ def main():
     st.write("Pipe network design")
     texto_ingresado = st.text_input("Ingrese su texto", "Texto predeterminado")
     st.write("Texto ingresado:", texto_ingresado)
+ st.title("Menú con 3 opciones")
 
+    opcion = st.sidebar.radio(
+        "Selecciona una opción:",
+        ("Opción 1", "Opción 2", "Opción 3")
+    )
+
+    if opcion == "Opción 1":
+        st.write("Has seleccionado la Opción 1")
+    elif opcion == "Opción 2":
+        st.write("Has seleccionado la Opción 2")
+    elif opcion == "Opción 3":
+        st.write("Has seleccionado la Opción 3")
 if __name__ == "__main__":
      main()

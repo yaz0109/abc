2315683619a4dc22e28402bba1ccb8c160dfb287
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 10:28:52 -0700

    Update max.py
    q

--- max.py
+++ max.py
@@ -4,14 +4,10 @@ import pandas as pd
 import math
 
 def main():
-
     st.title("Challenging situation")
     st.write("Pipe network stronghold")
     st.write("Pipe network design")
     texto_ingresado = st.text_input("Ingrese su texto", "Texto predeterminado")
     st.write("Texto ingresado:", texto_ingresado)
-    
-    
-    
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":

ade9620a85b6bc6315c729143ad0e4a03d76ac6c
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 11:06:37 -0700

    Update max.py
    M

--- max.py
+++ max.py
@@ -13,20 +13,15 @@ def main():
 
     opcion = st.sidebar.radio(
         "Selecciona una opción:",
-        ("INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS","AGRADECIMIENTOS"  )
- st.title("MENU")
+        ("INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS","AGRADECIMIENTOS")
+    )
 
-    if st.button("INTRODUCCIÓN"):
-        st.write("INTRODUCCIÓN:")
-    if st.button("PROGRAMA"):
-        st.write("PROGRAMA:")
-    if st.button("FUENTES BIBLIOGRAFICAS"):
-        st.write("FUENTES BIBLIOGRAFICAS")
     if opcion == "INTRODUCCIÓN":
         st.write("INTRODUCCIÓN:")
     elif opcion == "PROGRAMA":
         st.write("PROGRAMA:")
     elif opcion == "FUENTES BIBLIOGRAFICAS":
         st.write("FUENTES BIBLIOGRAFICAS:")
+
 if __name__ == "__main__":
-     main()
+    main()

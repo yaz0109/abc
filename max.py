7aaa109ef5e28cfd0a74a49b2383b643ee274cac
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 10:14:51 -0700

    Update max.py
    a

--- max.py
+++ max.py
@@ -4,9 +4,7 @@ import pandas as pd
 import math
 
 def main():
-tit = "Challenging situation"
-print(tit)
-print("=" * len(tit)) 
+
     st.title("Challenging situation")
     st.write("Aquí está tu código Python:")
 if __name__ == "__main__":

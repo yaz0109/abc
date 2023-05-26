21ccbc0df17536460dee95192056246f6552044d
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 13:14:52 -0700

    Update max.py
    a

--- max.py
+++ max.py
@@ -3,18 +3,18 @@ import numpy as np
 import pandas as pd
 import math
 #___________________________________
-import INTRODUCCIÓN as bs
+import INTRODUCCIÓN as itr
 def main():
 
     image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
-    st.image(image_url, use_column_width=True, caption="Imagen centrada")
+    st.image(image_url, use_column_width=True, caption=".")
 
     seleccion = st.selectbox("Seleccione una opción:", ["INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS"])
 
     if seleccion == "INTRODUCCIÓN":
         st.subheader("INTRODUCCIÓN")
 
-        bs.INTRODUCCIÓN()
+        itr.INTRODUCCIÓN()
 
     elif seleccion == "PROGRAMA":
         st.subheader("PROGRAMA")

0e17100a6fced2f8e3d9b5765b7ab7432fc6e789
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 12:40:08 -0700

    Update max.py
    P

--- max.py
+++ max.py
@@ -5,25 +5,25 @@ import math
 
 def main():
 
-    image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg" 
+    image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
     st.image(image_url, use_column_width=True, caption="Imagen centrada")
-    
-    seleccion = st.selectbox("Seleccione una opción: ", ["INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS"])
 
-if seleccion == "INTRODUCCIÓN": 
-    st.subheader("INTRODUCCIÓN")
-    st.latex('''x^3 - 6x^2 + 11x -6.1''')
-    bs.INTRODUCCIÓN()
+    seleccion = st.selectbox("Seleccione una opción:", ["INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS"])
 
-if seleccion == "PROGRAMA": 
-    st.subheader("PROGRAMA")
-    st.latex('''x^3 - 6x^2 + 11x -6.1''')
-    bs.INTRODUCCIÓN()
-    
-if seleccion == "FUENTES BIBLIOGRAFICAS": 
-    st.subheader("FUENTES BIBLIOGRAFICAS")
-    st.latex('''x^3 - 6x^2 + 11x -6.1''')
-    bs.FUENTESBIBLIOGRAFICAS()    
+    if seleccion == "INTRODUCCIÓN":
+        st.subheader("INTRODUCCIÓN")
+        st.latex('''x^3 - 6x^2 + 11x -6.1''')
+        bs.INTRODUCCIÓN()
+
+    elif seleccion == "PROGRAMA":
+        st.subheader("PROGRAMA")
+        st.latex('''x^3 - 6x^2 + 11x -6.1''')
+        bs.INTRODUCCIÓN()
+
+    elif seleccion == "FUENTES BIBLIOGRAFICAS":
+        st.subheader("FUENTES BIBLIOGRAFICAS")
+        st.latex('''x^3 - 6x^2 + 11x -6.1''')
+        bs.FUENTESBIBLIOGRAFICAS()  
         
         
     st.title("Menú:")

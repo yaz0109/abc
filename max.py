9c6f108cdc482d1eed898b7dce799d0787da3ff1
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-26 13:23:54 -0700

    Update max.py
    p

--- max.py
+++ max.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import math
 import INTRODUCCIÓN as it
-def main():
 
+def main():
     image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
     st.image(image_url, use_column_width=True, caption=".")
 
@@ -16,16 +16,11 @@ def main():
 
     elif seleccion == "PROGRAMA":
         st.subheader("PROGRAMA")
-
         bs.PROGRAMA()
 
     elif seleccion == "FUENTES BIBLIOGRAFICAS":
         st.subheader("FUENTES BIBLIOGRAFICAS")
-
         fb.FUENTESBIBLIOGRAFICAS()  
-        
-        
-   
 
 if __name__ == "__main__":
     main()

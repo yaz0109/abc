5bbca91c47b8e364ffefa89b60cf0763fb878219
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 22:11:48 -0700

    Update PROGRAMA.py
    ta

--- PROGRAMA.py
+++ PROGRAMA.py
@@ -5,4 +5,6 @@ import pandas as pd
 def PROGRAMA():
     st.title("DATOS DE ENTRADA")
     st.write("Resultado")
+    table_data = {'Column 1': [1, 2], 'Column 2': [3, 4]}
+    st.write(pd.DataFrame(data=table_data))
             

7aea9951e520911b556bdc682783d05352e90255
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 22:30:02 -0700

    Update PROGRAMA.py
    aaaaa

--- PROGRAMA.py
+++ PROGRAMA.py
@@ -6,5 +6,17 @@ def PROGRAMA():
     st.title("DATOS DE ENTRADA")
     st.write("Resultado")
     table_data = {'Column 1': [1, 2], 'Column 2': [3, 4]}
-    st.write(pd.DataFrame(data=table_data))
+    st.write(pd.DataFrame(data=table_data)) 
+    st.text('Fixed width text')
+    st.markdown('_Markdown_') # see *
+    st.caption('Balloons. Hundreds of them...')
+    st.latex(r''' e^{i\pi} + 1 = 0 ''')
+    st.write('Most objects') # df, err, func, keras!
+    st.write(['st', 'is <', 3]) # see *
+    st.title('My title')
+    st.header('My header')
+    st.subheader('My sub')
+    st.code('for i in range(8): foo()')
+
+
             

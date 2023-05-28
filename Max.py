8be7127b45ff7aafabf54f7da91acf12e4508c9b
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 21:54:26 -0700

    Update Max.py
    fb

--- Max.py
+++ Max.py
@@ -4,6 +4,7 @@ import pandas as pd
 import math
 import INTRODUCCION as io
 import PROGRAMA as bs
+import FUENTES as fb
 def main():
     image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
     st.image(image_url, use_column_width=True, caption=".")
@@ -20,7 +21,7 @@ def main():
 
     elif seleccion == "FUENTES BIBLIOGRAFICAS":
         st.subheader("FUENTES BIBLIOGRAFICAS")
-        fb.FUENTESBIBLIOGRAFICAS()  
+        fb.FUENTES()  
 
 if __name__ == "__main__":
     main()

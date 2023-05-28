4bb2a322c5d9d9bcb6fb14a593f082632475fc2e
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 21:57:04 -0700

    Update Max.py
    q

--- Max.py
+++ Max.py
@@ -5,6 +5,7 @@ import math
 import INTRODUCCION as io
 import PROGRAMA as bs
 import FUENTES as fb
+
 def main():
     image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
     st.image(image_url, use_column_width=True, caption=".")

725e1a0b3efabc95691cc301060cb38c546c082e
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 22:37:46 -0700

    Update PROGRAMA.py
    a

--- PROGRAMA.py
+++ PROGRAMA.py
@@ -5,8 +5,7 @@ import pandas as pd
 def PROGRAMA():
     st.title("DATOS DE ENTRADA")
     st.write("Resultado")
-    table_data = {'Column 1': [1, 2], 'Column 2': [3, 4]}
-    st.write(pd.DataFrame(data=table_data)) 
+    
     st.text('Fixed width text')
     st.markdown('_Markdown_') # see *
     st.caption('Balloons. Hundreds of them...')

5c63cb9257c85397f0b02285685cf81a463478ca
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-27 17:49:22 -0700

    Update INTRODUCCIÓN.py
    def

--- INTRODUCCIÓN.py
+++ INTRODUCCIÓN.py
@@ -1,14 +1,17 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.title("RED DE TUBERÍAS")
-st.write("Aquí, se proporciona un croquis isométrico de la red de tuberías como ayuda visual."
-         " Sin embargo, se necesitan algunas especificaciones para estas tuberías y "
-         "esta es la situación de diseño que debe enfrentar un ingeniero.")
-st.write("El objetivo de la red es suministrar agua en cuatro puntos "
-         "diferentes de una gran planta de proceso como parte de los servicios. "
-         "Se han ideado unas válvulas de compuerta en la red, como se muestra, "
-         "para que se pueda cortar el suministro en cualquiera de los cuatro puntos diferentes.")
-st.write("Se estableció la disposición de la red de tuberías al considerar "
-         "el equipo de proceso instalado, los soportes mecánicos de tubería "
-         "plausibles y la seguridad. "
-         "Sin embargo, no se determinaron los tamaños de las tuberías.")
+def INTRODUCCION():
+    st.title("RED DE TUBERÍAS")
+    st.write("Aquí, se proporciona un croquis isométrico de la red de tuberías como ayuda visual."
+             " Sin embargo, se necesitan algunas especificaciones para estas tuberías y "
+             "esta es la situación de diseño que debe enfrentar un ingeniero.")
+    st.write("El objetivo de la red es suministrar agua en cuatro puntos "
+             "diferentes de una gran planta de proceso como parte de los servicios. "
+             "Se han ideado unas válvulas de compuerta en la red, como se muestra, "
+             "para que se pueda cortar el suministro en cualquiera de los cuatro puntos diferentes.")
+    st.write("Se estableció la disposición de la red de tuberías al considerar "
+             "el equipo de proceso instalado, los soportes mecánicos de tubería "
+             "plausibles y la seguridad. "
+             "Sin embargo, no se determinaron los tamaños de las tuberías.")

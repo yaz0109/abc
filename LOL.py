db7858082989a16589cf91c25bd26b6c821c241d
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-29 17:43:54 -0600

    U

--- LOL.py
+++ LOL.py
@@ -205,7 +205,7 @@ def main():
       # Guardar los valores de diámetro interno en variables individuales
       ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8 = diametros_internos
 
-      def calcular_area_diametro(diametro):
+     def calcular_area_diametro(diametro):
           radio = diametro / 2
           area = math.pi * (radio ** 2)
           return area
@@ -228,11 +228,11 @@ def main():
       st.write("Valores de longitud de cada sección de tubería, diametro en función al caudal de alimentación, obtenidos de tablas tomando como criterio de diseño una velocidad constante para el agua (aproximadamente 1.5 m/s):")
       data = {
         "Tubería": ["1", "2", "3", "4", "5", "6", "7", "8"],
-      "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8],
-      "Caudal (m^3/s)": ["{:.8f}".format(Q1), "{:.8f}".format(Q2), "{:.8f}".format(Q3), "{:.8f}".format(Q4), "{:.8f}".format(Q5), "{:.8f}".format(Q6), "{:.8f}".format(Q7), "{:.8f}".format(Q8)],
-      "Diámetro (m)": [ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8],
-      "ND (m)": [diametro1, diametro2, diametro3, diametro4, diametro5, diametro6, diametro7, diametro8],
-       
+         "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8],
+         "Caudal (m^3/s)": ["{:.8f}".format(Q1), "{:.8f}".format(Q2), "{:.8f}".format(Q3), "{:.8f}".format(Q4), "{:.8f}".format(Q5), "{:.8f}".format(Q6), "{:.8f}".format(Q7), "{:.8f}".format(Q8)],
+         "Diámetro (m)": [ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8],
+         "ND (m)": [diametro1, diametro2, diametro3, diametro4, diametro5, diametro6, diametro7, diametro8],
+         "Área (m^2)": [A1, A2, A3, A4, A5, A6, A7, A8],
 
             }
     

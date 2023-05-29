09e24317f12bc32072e4aa279725845312c0faba
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-29 13:00:26 -0700

    Update LOL.py
    tabla

--- LOL.py
+++ LOL.py
@@ -41,10 +41,10 @@ def main():
       data = {
         "Tubería": ["1", "2", "3", "4", "5", "6", "7", "8"],
         "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8]
-    }
+            }
 
-    # Mostrar los valores en forma de tabla
-    st.table(data)
+      # Mostrar los valores en forma de tabla
+      st.table(data)
       # Definir la velocidad constante
       velocidad = 1.5
       # Declarar los valores para gravedad, rugosidad, y viscosidad

a7491f18372f976641c1cbd5bf14953ecbd402d0
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-29 17:13:41 -0600

    W

--- LOL.py
+++ LOL.py
@@ -231,12 +231,13 @@ def main():
         "Tubería": ["1", "2", "3", "4", "5", "6", "7", "8"],
       "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8],
       "Caudal (m^3/s)": ["{:.8f}".format(Q1), "{:.8f}".format(Q2), "{:.8f}".format(Q3), "{:.8f}".format(Q4), "{:.8f}".format(Q5), "{:.8f}".format(Q6), "{:.8f}".format(Q7), "{:.8f}".format(Q8)],
+      "Diámetro (m)": ["{:.4f}".format(ID1), "{:.4f}".format(ID2), "{:.4f}".format(ID3), "{:.4f}".format(ID4), "{:.4f}".format(ID5), "{:.4f}".format(ID6), "{:.4f}".format(ID7), "{:.4f}".format(ID8)],
 
             }
     
       # Mostrar los valores en forma de tabla
       st.table(data)
-      
+
       st.write("Con todos estos valores se calculan los caudales reales de cada rama, para eso se hace uso del método de Hardy Cross")
       st.write()
 

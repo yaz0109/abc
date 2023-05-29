d12c272f5cb97467cc7d6e75949626407a92d703
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-29 17:45:32 -0600

    S

--- LOL.py
+++ LOL.py
@@ -205,21 +205,21 @@ def main():
       # Guardar los valores de diámetro interno en variables individuales
       ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8 = diametros_internos
 
-     def calcular_area_diametro(diametro):
+      def calcular_area_diametro(diametro):
           radio = diametro / 2
           area = math.pi * (radio ** 2)
           return area
 
 
-          A1 = calcular_area_diametro(ID1)
-          A2 = calcular_area_diametro(ID2)
-          A3 = calcular_area_diametro(ID3)
-          A4 = calcular_area_diametro(ID4)
-          A5 = calcular_area_diametro(ID5)
-          A6 = calcular_area_diametro(ID6)
-          A7 = calcular_area_diametro(ID7)
-          A8 = calcular_area_diametro(ID8)
-
+      A1 = calcular_area_diametro(ID1)
+      A2 = calcular_area_diametro(ID2)
+      A3 = calcular_area_diametro(ID3)
+      A4 = calcular_area_diametro(ID4)
+      A5 = calcular_area_diametro(ID5)
+      A6 = calcular_area_diametro(ID6)
+      A7 = calcular_area_diametro(ID7)
+      A8 = calcular_area_diametro(ID8)
+      
       # Imprimir los valores de diámetro interno
 
       st.write()
@@ -228,11 +228,10 @@ def main():
       st.write("Valores de longitud de cada sección de tubería, diametro en función al caudal de alimentación, obtenidos de tablas tomando como criterio de diseño una velocidad constante para el agua (aproximadamente 1.5 m/s):")
       data = {
         "Tubería": ["1", "2", "3", "4", "5", "6", "7", "8"],
-         "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8],
-         "Caudal (m^3/s)": ["{:.8f}".format(Q1), "{:.8f}".format(Q2), "{:.8f}".format(Q3), "{:.8f}".format(Q4), "{:.8f}".format(Q5), "{:.8f}".format(Q6), "{:.8f}".format(Q7), "{:.8f}".format(Q8)],
-         "Diámetro (m)": [ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8],
-         "ND (m)": [diametro1, diametro2, diametro3, diametro4, diametro5, diametro6, diametro7, diametro8],
-         "Área (m^2)": [A1, A2, A3, A4, A5, A6, A7, A8],
+      "Longitud (m)": [L1, L2, L3, L4, L5, L6, L7, L8],
+      "Caudal (m^3/s)": ["{:.8f}".format(Q1), "{:.8f}".format(Q2), "{:.8f}".format(Q3), "{:.8f}".format(Q4), "{:.8f}".format(Q5), "{:.8f}".format(Q6), "{:.8f}".format(Q7), "{:.8f}".format(Q8)],
+      "Diámetro (m)": [ID1, ID2, ID3, ID4, ID5, ID6, ID7, ID8],
+      "ND (m)": [diametro1, diametro2, diametro3, diametro4, diametro5, diametro6, diametro7, diametro8],
 
             }
     
@@ -243,7 +242,20 @@ def main():
       st.write()
 
 
-      
+      def calcular_area_diametro(diametro):
+          radio = diametro / 2
+          area = math.pi * (radio ** 2)
+          return area
+
+
+      A1 = calcular_area_diametro(ID1)
+      A2 = calcular_area_diametro(ID2)
+      A3 = calcular_area_diametro(ID3)
+      A4 = calcular_area_diametro(ID4)
+      A5 = calcular_area_diametro(ID5)
+      A6 = calcular_area_diametro(ID6)
+      A7 = calcular_area_diametro(ID7)
+      A8 = calcular_area_diametro(ID8)
 
       st.write("Áreas de los diámetros:")
       st.write("Tubo 1:", A1,"mts^2")

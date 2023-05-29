2c7b001134878556410faf809cfcf37d0bbdf069
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-28 20:16:34 -0700

    Update INTRODUCCION.py
    true

--- INTRODUCCION.py
+++ INTRODUCCION.py
@@ -19,7 +19,7 @@ def INTRODUCCION():
     
     image_cau = "https://i.pinimg.com/564x/9e/51/ad/9e51ad004a6af9462cdcf6f3f8ec1464.jpg"
     image_width = st.image(image_cau, width=100, use_column_width=True, caption="Balance Global.").width
-    st.image(image_cau, width=20, caption="Balance global")
+    st.image(image_cau, width=20,  use_column_width=True, caption="Balance global")
 
 if __name__ == '__main__':
     INTRODUCCION()

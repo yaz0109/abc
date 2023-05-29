6ddac2812e8df572a853cc29ee113f833bec9506
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-28 19:35:52 -0700

    Update INTRODUCCION.py
    balance img

--- INTRODUCCION.py
+++ INTRODUCCION.py
@@ -17,7 +17,8 @@ def INTRODUCCION():
     st.write("Este método consiste en el análisis de un sistema de tuberías empleando procedimiento de iteraciones. Para la aplicación de este método se requiere realizar un balance global de masa y un balance de pérdidas en cada circuito igualado a cero.")
     st.markdown("La biblioteca está ajustada y deseñada con el fin determinar la presión, HGL diámetro nominal, interno y tasas de flujo. Como se mostrara a continuación en la sección **PROGRAMA**.")
     
-
-        
+    image_cau = "https://i.pinimg.com/564x/a1/af/5c/a1af5c89259012b9f17748685cd76685.jpg"
+    st.image(image_cau, use_column_width=True, caption=".")
+    st.markdown('_Balance global de masa._') # see *    
 if __name__ == '__main__':
     INTRODUCCION()

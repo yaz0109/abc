77859f2e2db84034cec73533bbdb41ea8899be5b
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 10:29:54 -0600

    css

--- ESPECIFICACIONES.py
+++ ESPECIFICACIONES.py
@@ -6,8 +6,12 @@ def ESPECIFICACIONES ():
 
     
     
-    st.markdown(f"<h2 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h2>", unsafe_allow_html=True)
+    
+    st.markdown(f"<h2    style='text-align: center;'> ¿Por qué tubería de acero? </h>", unsafe_allow_html=True)
 
 
+    st.write ("Existen varias razones por las que se utilizará el acero como material para tuberías en las redes de tubería. Algunas de las razones son las siguientes:")
+    st.write ("1.	Resistencia: El acero es un material muy resistente y duradero. Tiene una alta resistencia a la tracción, lo que significa que puede soportar una gran cantidad de peso y tensión sin deformarse o romperse.")
 if __name__ == '__main__':
     ESPECIFICACIONES()

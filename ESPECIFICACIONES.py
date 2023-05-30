481bdd21e4c85bf6a0860cb9d4788c1063481a43
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 10:14:48 -0600

    WFG

--- ESPECIFICACIONES.py
+++ ESPECIFICACIONES.py
@@ -4,3 +4,4 @@ import pandas as pd
 
 def ESPECIFICACIONES():
     st.title("ESPECIFICACIONES DE LA RED TUBERÍAS ")
+    st.markdown(f"<h3 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h3>", unsafe_allow_html=True)

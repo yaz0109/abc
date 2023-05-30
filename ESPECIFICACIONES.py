555b24ef181ab3c9697e1065fa2c05e0b25f0abe
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 10:18:43 -0600

    sd

--- ESPECIFICACIONES.py
+++ ESPECIFICACIONES.py
@@ -2,6 +2,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-def ESPECIFICACIONES():
-    st.title("ESPECIFICACIONES DE LA RED TUBERÍAS ")
-    st.markdown(f"<h3 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h3>", unsafe_allow_html=True)
+def main ():
+
+    def ESPECIFICACIONES():
+        st.title("ESPECIFICACIONES DE LA RED TUBERÍAS ")
+        st.markdown(f"<h3 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h3>", unsafe_allow_html=True)
+
+        
+if __name__ == "__main__":
+    main()

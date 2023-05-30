bb6024efbecd26dfd2227878fd2940d90d6cae54
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 10:31:51 -0600

    fd

--- ESPECIFICACIONES.py
+++ ESPECIFICACIONES.py
@@ -8,10 +8,16 @@ def ESPECIFICACIONES ():
     
     st.markdown(f"<h2 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h2>", unsafe_allow_html=True)
     
-    st.markdown(f"<h2    style='text-align: center;'> ¿Por qué tubería de acero? </h>", unsafe_allow_html=True)
-
+    st.markdown(f"<h3    style='text-align: center;'> ¿Por qué tubería de acero? </h3>", unsafe_allow_html=True)
+    st.write ()
 
     st.write ("Existen varias razones por las que se utilizará el acero como material para tuberías en las redes de tubería. Algunas de las razones son las siguientes:")
     st.write ("1.	Resistencia: El acero es un material muy resistente y duradero. Tiene una alta resistencia a la tracción, lo que significa que puede soportar una gran cantidad de peso y tensión sin deformarse o romperse.")
+    st.write ("2.	Durabilidad: Las tuberías de acero son resistentes a la corrosión y pueden durar décadas sin necesidad de mantenimiento. Esto las hace ideales para su uso en aplicaciones de alta presión y temperatura.")
+    st.write ("3.	Seguridad: Las tuberías de acero son muy seguras, ya que son resistentes al fuego y no se queman fácilmente. También son capaces de soportar terremotos y otros desastres naturales, lo que las convierte en una opción ideal para sistemas de tuberías en áreas de riesgo sísmico.")
+    st.write ("4.	Economía: Aunque el costo inicial de la instalación de tuberías de acero puede ser mayor que el de otros materiales, su durabilidad y resistencia a largo plazo pueden hacer que sean más económicas a largo plazo. Además, el acero es fácil de fabricar y transportar, lo que puede ayudar a reducir los costos.")
+    st.write ("5.	Versatilidad: Las tuberías de acero se pueden utilizar en una amplia variedad de aplicaciones, incluyendo sistemas de agua potable, gasoductos, oleoductos, sistemas de calefacción y refrigeración, entre otros. También pueden ser fabricadas en una variedad de tamaños y formas para adaptarse a las necesidades específicas de cada proyecto.")
+
+        
 if __name__ == '__main__':
     ESPECIFICACIONES()

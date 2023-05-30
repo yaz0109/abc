92d8bf15ecbb853443fcff413fcbfa564a115fb0
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 11:37:55 -0600

    d

--- FUENTES.py
+++ FUENTES.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 
 def FUENTES():
-    st.title("FUENTES BIBLIOGRAFICAS")
-    st.markdown(f"<h2 style='text-align: center;'> ESPECIFICACIONES DE LA RED TUBERÍAS </h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: center;'> FUENTES BIBLIOGRAFICAS </h2>", unsafe_allow_html=True)
     st.write ()
 
 

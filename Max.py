883257fdcb3e91955c7280c3cb56288e32f5ac23
yaz0109 <102831791+yaz0109@users.noreply.github.com>
2023-05-30 09:28:54 -0700

    Update Max.py
    ef

--- Max.py
+++ Max.py
@@ -4,6 +4,7 @@ import pandas as pd
 import math
 import INTRODUCCION as io
 import LOL as la
+import ESPECIFICACIOES as ef
 import FUENTES as fb
 
 
@@ -11,7 +12,7 @@ def main():
     image_url = "https://i.pinimg.com/564x/a7/db/1c/a7db1c37805f8ddb1a34f70c94f4784e.jpg"
     st.image(image_url, use_column_width=True, caption=".")
 
-    seleccion = st.selectbox("Seleccione una opción:", ["INTRODUCCIÓN", "PROGRAMA", "FUENTES BIBLIOGRAFICAS"])
+    seleccion = st.selectbox("Seleccione una opción:", ["INTRODUCCIÓN", "PROGRAMA","ESPECIFICACIONES", "FUENTES BIBLIOGRAFICAS"])
 
     if seleccion == "INTRODUCCIÓN":
         st.subheader("INTRODUCCIÓN")
@@ -21,7 +22,10 @@ def main():
         st.subheader("PROGRAMA")
         la.LOL()
         
-
+    elif seleccion == "ESPECIFICACIONES":
+        st.subheader("ESPECIFICACIONES")
+        ef.ESPECIFICACIONES()  
+        
     elif seleccion == "FUENTES BIBLIOGRAFICAS":
         st.subheader("FUENTES BIBLIOGRAFICAS")
         fb.FUENTES()  
